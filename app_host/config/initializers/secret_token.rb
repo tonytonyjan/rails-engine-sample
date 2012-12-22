9441ed7abb1b350d16ee80d1e5100d98b6cb219b
Tony Jian <tonytonyjan@gmail.com>
2012-12-22 14:51:10 +0800

    create 3 rails apps.

---
+++ app_host/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppHost::Application.config.secret_token = 'a82205a0203eff13b5ab7ec25c98efdabb415846610faea702b4925daabaf0d319b9f6557e45b1bdb10b3d9532dcfc4ea3fd6db8ae9972b090e98fc5b5185446'
---
+++ app_mountable/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppMountable::Application.config.secret_token = '8f17181ab31443d674a46745ee831b206965ac8156bba8c021995dc9515a0fa4880a498f642a1cbb3fc5afb4e68b93f0a91cc9321381ea472ba12fee87b5619f'
